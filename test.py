beb22506cbc8d1816b6c1a2a8e37e77854f7aac5
Robin Clowers <robin.clowers@gmail.com>
2020-02-29 18:45:21 -0800

    Add add function

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+import math
+
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual(math.add(2, 5), 7)
+
+if __name__ == '__main__':
+    unittest.main()
